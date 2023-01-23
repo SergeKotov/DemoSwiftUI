ce96fd2d0ebe09c3ddaf907364d58b7e2c62e9ae
SergeKotov <serge.kotov@gmail.com>
2023-01-23 12:04:31 +0300

    Initial Commit

---
+++ DemoSwiftUI/DemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftUIApp.swift
+//  DemoSwiftUI
+//
+//  Created by Serge Kotov on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
